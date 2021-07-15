b1bb16a819caa142cf6f4f1dcdb96ff4f696be8b
anamata <ax.piotr@gmail.com>
2021-07-15 22:00:38 +0200

    aisopl

---
+++ js/main.js
@@ -0,0 +1,46 @@
+
+$(document).ready(
+
+    function() {
+
+        $("article > a").on( "click", function(e) {
+            
+            /* oznacz wybrany produkt */
+            $( this ).parent().toggleClass("klikniete"); 
+            
+            /* overlay on + show animation */
+            $(".overlay").addClass("showoverlay anim1"); 
+            
+            /* pasek od scrollowania listy produktow na razie niepotrzebny*/
+            //TODO dodawac to tylko gdy jest scrollbar 
+            $("body").addClass("hidescrollbar"); 
+
+
+            //$("main").toggleClass("kliknietymain")
+
+
+            e.preventDefault();
+          });
+        $(".closebtn").on( "click", function(e) {
+            $( this ).parent().removeClass("klikniete");
+            //$("main").toggleClass("kliknietymain");
+            $("body").removeClass("hidescrollbar");
+            $(".overlay").removeClass("showoverlay");
+            //$("main").show();
+
+            e.preventDefault();
+        });  
+    }
+)
+
+
+
+/*
+click(function(){alert("sdas")})
+
+
+function klikniete(e: Event) {
+
+    alert("kliklem");
+    e.preventDefault();
+}*/
